648add7a2251eb30c6cb01fd3f303f0620fab66e
luliannovi <48823939+luliannovi@users.noreply.github.com>
2022-08-16 17:25:24 +0200

    Delete Twitter_API directory

--- Twitter_API/src/main/java/com/example/twitter_api/prova_api.java
+++
@@ -1,5 +0,0 @@
-package com.example.twitter_api;
-
-public class prova_api {
-
-}
--- Twitter_API/src/main/java/module-info.java
+++
@@ -1,6 +0,0 @@
-module com.example.twitter_api {
-    requires javafx.controls;
-    requires javafx.fxml;
-    opens com.example.twitter_api to javafx.fxml;
-    exports com.example.twitter_api;
-}
